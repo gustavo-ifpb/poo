7faf8a990aa3a4b361c8ffea018958170e6e533a
Gustavo Soares <gustavo@papaga.io>
2019-09-03 08:13:25 -0300

    write/read csv

--- code/files/csv.py
+++ code/files/csv.py
@@ -1,9 +1,26 @@
+# Leitura
 with open('pessoas.csv', 'r') as f:
     # print(f.readlines())
 
     headers = f.readline().split(';')
     print(headers)
 
+    selecionados = []
+
     for line in f:
-        for i, data in enumerate(line.split(';')):
-            print(f'{headers[i]}: {data}')
+        data = line.split(';')
+        op = input(f'Deseja selecionar a pessoa {data[0]}? (s\\n) ')
+        if op.lower() == 's':
+            selecionados.append('{};{}'.format(data[0], data[2]))
+
+        # for i, data in enumerate(line.split(';')):
+        #     print(f'{headers[i]}: {data}')
+
+# Escrita
+with open('pessoas_out.csv', 'w') as fout:
+    # Header
+    fout.write('Nome;E-mail\n')
+    for selecionado in selecionados:
+        fout.write(selecionado)
+        fout.write('\n')
+    fout.close()
