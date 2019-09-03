c5fd88624d588cd2172928107c4f2eefe021679c
Gustavo Soares <gustavo@papaga.io>
2019-09-03 07:53:41 -0300

    json

--- code/files/pessoas_json.py
+++ code/files/pessoas_json.py
@@ -1,6 +1,23 @@
 import json
 
+# Leitura
 f = open('pessoas.json', 'r')
 pessoas = json.load(f)
+
+selecionados = {'pessoas': []}
+
 for pessoa in pessoas['pessoas']:
-    print(f'Nome: {pessoa["nome"]}')
+    op = input(f'Deseja selecionar a pessoa {pessoa["nome"]}? (s\\n) ')
+    if op.lower() == 's':
+        novaIdade = pessoa['idade'] + 10
+        pessoaSelecionada = {
+            'nome': pessoa['nome'],
+            'e-mail': pessoa['email'],
+            'idade': novaIdade
+        }
+        selecionados['pessoas'].append(pessoaSelecionada)
+    
+print(selecionados)
+# Escrita
+fout = open('pessoas_out.json', 'w')
+json.dump(selecionados, fout)
